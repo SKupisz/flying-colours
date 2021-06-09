6825804f881943a411bca3d4006e5759d4b34e36
SKupisz <szym-ku@wp.pl>
2021-06-09 10:36:05 +0200

    Designed the welcome site with its RWD

--- resources/js/components/Welcome.js
+++ resources/js/components/Welcome.js
@@ -1,11 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Wave } from "react-animated-text";
+import { Grid, Button } from "@material-ui/core";
 
 export default class Welcome extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.data = require("./data/welcome.json");
+        this.shortDesc = this.data.shortdesrcibe.split(" ");
+    }
     render(){
-        return <div className="signin-container welcome-container">
+        return <Grid container xs={12} className="signin-container welcome-container">
             <header className="main-header block-center">Welcome to Flying Colours</header>
-        </div>;
+            <div className = "short-describe block-center">
+                {this.shortDesc.map(elem => <div className = "short-desc-word">
+                    <Wave text = {elem+" "}
+                    effect = "verticalFadeIn" iterations = {1}
+                    paused = {false} effectDuration = {0.5} delay = {0.2}
+                    effectChange = {0.5} effectDelay = {1} className = "short-describe-wrapper"/>
+                </div>)}
+            </div> 
+            <Grid item xs={12} className = "buttons-section block-center">
+                {this.data.links.map(elem => <a href = {elem["href"]}>
+                    <Button variant="contained" 
+                    className="section-btn" type = "button">
+                        {elem["content"]}
+                    </Button>
+                </a>)}
+            </Grid>
+        </Grid>;
     }
 }
 

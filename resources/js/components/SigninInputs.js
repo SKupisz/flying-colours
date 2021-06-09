7a9157c6b139a1057278ea2d1892e9dc36701413
SKupisz <szym-ku@wp.pl>
2021-06-09 10:45:07 +0200

    Added RWD to Sign in and Sign up sites

--- resources/js/components/SigninInputs.js
+++ resources/js/components/SigninInputs.js
@@ -11,31 +11,31 @@ export default class SigninInputs extends React.Component{
         };
     }
     render(){
-        return <Grid container className="sign-in-container block-center">
-            {this.state.statusOfTheSigningUp === 1 ?  <Grid item sm={12}>
+        return <Grid container className="sign-in-container block-center" xs={12}>
+            {this.state.statusOfTheSigningUp === 1 ?  <Grid item xs={12}>
                 <TextField
                 required label="Nickname" variant="filled" 
                 className = "email-input block-center" margin="normal" name="nick" type = "text"/>
             </Grid>: ""}
-            <Grid item sm={12}>
+            <Grid item xs={12}>
                 <TextField
                 required label="Email" variant="filled" 
                 className = "email-input block-center" margin="normal" name="email" type = "email"/>
             </Grid>
-            <Grid item sm={12}>
+            <Grid item xs={12}>
                 <TextField
                 required label="Password" variant="filled" 
                 className = "email-input block-center" margin="normal" name="passwd" type = "password"/>
             </Grid>
-            {this.state.statusOfTheSigningUp === 1 ? <Grid item sm={12}>
+            {this.state.statusOfTheSigningUp === 1 ? <Grid item xs={12}>
                 <TextField
                 required label="Repeat password" variant="filled" 
                 className = "email-input block-center" margin="normal" name="passwd_rep" type = "password"/>
             </Grid>: ""}
-            {this.state.errorMessage.length === 0 ? "" : <Grid item sm={12}>
+            {this.state.errorMessage.length === 0 ? "" : <Grid item xs={12}>
                 <div className="error-message block-center">{this.state.errorMessage}</div>
                 </Grid>}
-            <Grid item sm={12}>
+            <Grid item xs={12}>
                 <Button type = "submit" className = "login-btn block-center">Sign in</Button>
             </Grid>
         </Grid>;
